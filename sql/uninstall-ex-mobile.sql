aac6be32b792f8b4db4babc17963ee4984f034c0
ajaxbo360 <ajakak.2016@gmail.com>
2024-04-10 01:15:04 +0000

    Revert "create vmobile module"
    This reverts commit d74b32e24e0fdc1e925015012cbc1416e39ee80b.

--- sql/uninstall-ex-mobile.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `ps_vala_compatibility`;
-DROP TABLE IF EXISTS `ps_vala_mobile`;
-DROP TABLE IF EXISTS `ps_vala_mobile_lang`;
-DROP TABLE IF EXISTS `ps_vala_mobile_type`;
-DROP TABLE IF EXISTS `ps_vala_mobile_type_lang`;
-DROP TABLE IF EXISTS `ps_vala_manufacturer`;
-
-SET SESSION sql_mode='';
-SET NAMES 'utf8mb4';
